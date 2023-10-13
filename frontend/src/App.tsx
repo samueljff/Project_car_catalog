647fea8bbeed49af4da8637b4ed346e32119f866
samueljff <samueljosef3@gmail.com>
2023-10-12 21:50:06 -0300

    Initial Project

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>Trabalho Capitulo 01</h1>
+    </div>
+  );
+};
+
+export default App;
