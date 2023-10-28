40c6beddc041638b9f3d6705358daf3b6fb16ddc
samueljff <samueljosef3@gmail.com>
2023-10-28 00:02:05 -0300

    Fix: ajuste na Navbar

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -5,7 +5,7 @@ import { NavLink, Link} from 'react-router-dom';
 
 const Navbar = () => {
   return (
-    <nav className="navbar navbar-expand-sm bg-primary  main-nav">
+    <nav className="navbar navbar-expand-md bg-primary  main-nav">
       <div className="container-fluid">
         <Link to="/" className="nav-logo-text">
           <h4>Carros Top</h4>
